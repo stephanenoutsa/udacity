cbca5fcc47e38ac039f3e94674278f57697e7d29
Stephane Noutsa <stephanenoutsa@yahoo.com>
2020-01-02 17:56:50 +0100

    Adds important code which was omitted from initial fork

--- course-04/exercises/c4-client-master/src/config.ts
+++ course-04/exercises/c4-client-master/src/config.ts
@@ -1,5 +1,5 @@
-const apiId = 'nhmk625tw0'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'dcj0l5gr22'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'test-endpoint.auth0.com',
---
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -0,0 +1,2 @@
+const apiId = "dcj0l5gr22";
+export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`;
